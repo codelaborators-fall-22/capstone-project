37cc696c4052d76ff35fc416fd76025d055d18a7
AlliFolluo <arf7w6@gmail.com>
2022-11-29 21:08:29 -0500

    Built Keto Model

---
+++ application/src/main/java/io/codelaborators/serverside/models/Keto.java
@@ -0,0 +1,34 @@
+package io.codelaborators.serverside.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+
+public class Keto extends Recipe {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String imageUrl;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public Keto(int prepTime, String difficultyLevel, List<String> ingredients, int servingSize, int cookTime, int calories, int fat, int protein, int sugar, int sodium, int carbs, List<String> steps, String imageUrl) {
+        super(prepTime, difficultyLevel, ingredients, servingSize, cookTime, calories, fat, protein, sugar, sodium, carbs, steps);
+        this.imageUrl = imageUrl;
+    }
+
+    public Keto() {
+    }
+}
