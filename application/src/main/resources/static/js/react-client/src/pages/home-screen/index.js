c421461199110db8a8519cda3fdb31e875510a73
AlliFolluo <arf7w6@gmail.com>
2022-12-17 17:11:13 -0500

    in class front end

--- application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
@@ -1,52 +1,57 @@
 import React from 'react';
-
+import spoonsHeroImg from '../../assets/pexels-pixabay-357743.jpeg';
+import style from './style.module.scss';
 
 const HomeScreen = () => {
   return (
     <div>
-        <div className="subheader">
+        <div className={style.subheader}>
+          <img src={spoonsHeroImg}  alt="spoons hero image"/>
           <h2>Website Name</h2>
           <h3>Description of website</h3>
         </div>
           
-        <div class="dairyfree">
-            <div class="dairyFreeContainer">
+        <div className={style.recipeCategories}>
+            <div className={style.categoryContainer}>
+              <img src="insert dairy free img here" alt="dairy free recipe image" className="dairyFreeImg"/>
               <h4>Dairy Free Recipes</h4>
               <p>Dairy Free Description</p>
-              <button class="homeScreenRecipeCategoryBtn">Click here to see dairy-free recipes →</button>
+              <button className={style.homeScreenRecipeCatBtn}>See more →</button>
             </div>
-            <img src="insert dairy free img here" alt="dairy free recipe image"></img>
-        </div>
-        <div class="glutenfree">
-            <img src="insert gluten free img here" alt="gluten free recipe image"></img>
-            <div class="glutenFreeContainer">
+       
+            <div className={style.categoryContainer}>
+              <img src="gluten free" alt="gluten free recipe image" className="glutenFreeImg"/>
               <h4>Gluten Free Recipes</h4>
               <p>Gluten Free Description</p>
-              <button class="homeScreenRecipeCategoryBtn">Click here to see gluten-free recipes →</button>
-            </div>
-        </div>
-        <div class="keto">
-            <div class="ketoContainer">
+              <button className={style.homeScreenRecipeCatBtn}>See more →</button>
+              </div>
+  
+            <div className={style.categoryContainer}>
+            <img src="insert keto img here" alt="keto recipe image" className="ketoImg"/>
               <h4>Keto Recipes</h4>
               <p>Keto Description</p>
-              <button class="homeScreenRecipeCategoryBtn">Click here to see keto recipes →</button>
+              <button className={style.homeScreenRecipeCatBtn}>See more →</button>
             </div>
-            <img src="insert keto img here" alt="keto recipe image"></img>
-        </div>
-        <div class="mediterranean">
-            <img src="insert mediterranean img here" alt="mediterranean recipe image"></img>
-            <div class="mediterraneanContainer">
+      
+            <div className={style.categoryContainer}>
+              <img src="insert mediterranean img here" alt="mediterranean recipe image" className="mediterraneanImg"/>
               <h4>Mediterranean Recipes</h4>
               <p>Mediterranean Description</p>
-              <button class="homeScreenRecipeCategoryBtn">Click here to see mediterranean recipes →</button>
+              <button className={style.homeScreenRecipeCatBtn}>See more →</button>
             </div>
-        </div>
-        <div class="paleo">
-            <div class="paleoContainer">
-                <h4>Paleo Recipes</h4>
-                <p>Paleo Description</p>
-                <button class="homeScreenRecipeCategoryBtn">Click here to see paleo recipes →</button>
-            <img src="insert paleo img here" alt="paleo recipe image"></img>
+        
+            <div className={style.categoryContainer}>
+              <img src="insert paleo img here" alt="paleo recipe image" className="paleoImg"/>
+              <h4>Paleo Recipes</h4>
+              <p>Paleo Description</p>
+              <button className={style.homeScreenRecipeCatBtn}>See more →</button>
+            </div>
+
+            <div className={style.categoryContainer}>
+              <img src="insert vegan img here" alt="vegan recipe image" className="veganImg"/>
+              <h4>Vegan Recipes</h4>
+              <p>Vegan Description</p>
+              <button className={style.homeScreenRecipeCatBtn}>See more →</button>
             </div>
         </div>
     </div>
