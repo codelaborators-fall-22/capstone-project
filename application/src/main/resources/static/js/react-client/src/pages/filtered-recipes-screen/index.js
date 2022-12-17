27daa7399c350a8d8a71e23864f2c8d6fa13f94f
AlliFolluo <arf7w6@gmail.com>
2022-12-17 09:50:11 -0500

    create homepage buttons and recipe table

--- application/src/main/resources/static/js/react-client/src/pages/filtered-recipes-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/filtered-recipes-screen/index.js
@@ -3,7 +3,13 @@ import React from 'react';
 const FilteredRecipesScreen = () => {
   return (
     <div>
-      This is the filtered recipes screen
+      <table>
+        <button class="recipeScreenRecipeCategoryBtn">Dairy Free</button>
+        <button class="recipeScreenRecipeCategoryBtn">Gluten Free</button>
+        <button class="recipeScreenRecipeCategoryBtn">Keto</button>
+        <button class="recipeScreenRecipeCategoryBtn">Mediterranean</button>
+        <button class="recipeScreenRecipeCategoryBtn">Paleo</button>
+      </table>
     </div>
   );
 }
